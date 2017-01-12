e202d08e05b99c5f1835ffb563fdf34be0bc8282
Matthew Walls <matthew.walls@guardian.co.uk>
2017-01-12 15:10:06 +0000

    Parameterise the queue name

--- trousers/trousers.py
+++ trousers/trousers.py
@@ -5,10 +5,10 @@ import traceback, logging
 from requests.auth import HTTPBasicAuth
 
 ARTIFACTS_DIR = '/home/ubuntu/workspace/screenshots'
-QUEUE_NAME = 'trousers_in';
 BUCKET_NAME = 'prbuilds'
 GH_NAME = os.getenv('GH_NAME', '')
 GH_TOKEN = os.getenv('GH_TOKEN', '')
+QUEUE_NAME = os.getenv('QUEUE_NAME', '')
 
 class PullRequest:
     def __init__(self, obj):
@@ -201,6 +201,7 @@ if __name__ == '__main__':
     try:
         os.environ['GH_NAME']
         os.environ['GH_TOKEN']
+        os.environ['QUEUE_NAME']        
     except:
         sys.exit("Environment variables missing")
 
