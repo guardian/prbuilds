45e2f87c6a4390e0951c35e719ed2c4b6c7f6207
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-05-04 10:40:55 +0100

    Add `validate-a11y` step (v2)

---
+++ trousers/modules/a11yvalidation.py
@@ -0,0 +1,29 @@
+import subprocess
+import os
+
+class A11YValidation:
+
+    def run(self, directories, params):
+
+        """ run accessibility check validation """
+
+        ps = subprocess.Popen(
+            'bash -c ". ~/.nvm/nvm.sh; nvm use; make validate-a11y"',
+            shell=True,
+            stdout=subprocess.PIPE,
+            cwd=directories.workspace
+        )
+
+        out, err = ps.communicate()
+
+        open(
+            os.path.join(
+                directories.artifacts,
+                "a11y-results.txt"
+            ),"w"
+        ).write(out)
+
+        return {
+            "return_code": ps.returncode,
+            "raw_output": out
+        }
