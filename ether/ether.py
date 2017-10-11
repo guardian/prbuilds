22a52a5a8ec5c730d8c5ae3176527b6f583cc376
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-11 14:29:40 +0100

    Support for ether to return a liveblog

--- ether/ether.py
+++ ether/ether.py
@@ -3,9 +3,29 @@
 from flask import Flask
 app = Flask(__name__)
 
+default = "article"
+
+slugmap = {
+    "nobel-prize-in-economics-due-to-be-unveiled-business-live": "liveblog",
+    "guardian-journalists-jonathan-freedland-ghaith-abdul-ahad-win-orwell-prize-journalism": "article"
+}
+
+def handle(slug):
+
+    if slug in slugmap:
+        print "returning %s" % slugmap[slug]
+        return open("data/%s" % slugmap[slug], "r").read()
+    else:
+        print "returning default"
+        return open("data/%s" % default, "r").read()
+
+@app.route('/<category>/<subcategory>/<year>/<mon>/<day>/<slug>')
+def subcat(category, subcategory, year, mon, day, slug):
+    return handle(slug)
+
 @app.route('/<category>/<year>/<mon>/<day>/<slug>')
 def article(category, year, mon, day, slug):
-    return open("data/article", "r").read()
+    return handle(slug)
 
 if __name__ == '__main__':
     app.run(debug=False, port=9001)
