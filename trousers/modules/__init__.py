491392d369ee300f67e46a515ea27a86ffd048a2
Matthew Walls <matthew.walls@guardian.co.uk>
2018-02-08 10:59:52 +0000

    Don't fail completely if you fail one check

--- trousers/modules/__init__.py
+++ trousers/modules/__init__.py
@@ -22,12 +22,13 @@ allChecks = {
 def run_test(name, directories, params):
     try:
         print "Running %s" % k        
-        allChecks[name].run(
+        return allChecks[name].run(
             directories,
             params
         )
     except:
         print "Failed to run check: %s" % name
+        return None
 
 def run_with_config(chkConfig):
 
@@ -37,10 +38,13 @@ def run_with_config(chkConfig):
 
     for k, v in chkConfig.items():
         if k in allChecks.keys():
-            results[k] = run_test(
+            res = run_test(
                 name,
                 directories,
                 v
             )
+
+            if res is not None:
+                results[k] = res
 
     return results
