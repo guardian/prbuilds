221024fb7ecd79582215fc8d0ce3422e620b08e7
Matthew Walls <matthew.walls@guardian.co.uk>
2018-02-08 10:57:56 +0000

    Don't fail completely if you fail one check

--- trousers/modules/__init__.py
+++ trousers/modules/__init__.py
@@ -19,6 +19,16 @@ allChecks = {
     "lighthouse": LightHouseCheck()
 }
 
+def run_test(name, directories, params):
+    try:
+        print "Running %s" % k        
+        allChecks[name].run(
+            directories,
+            params
+        )
+    except:
+        print "Failed to run check: %s" % name
+
 def run_with_config(chkConfig):
 
     results = {}
@@ -27,10 +37,10 @@ def run_with_config(chkConfig):
 
     for k, v in chkConfig.items():
         if k in allChecks.keys():
-            print "Running %s" % k
-            results[k] = allChecks[k].run(
+            results[k] = run_test(
+                name,
                 directories,
-                params=v
+                v
             )
 
     return results
